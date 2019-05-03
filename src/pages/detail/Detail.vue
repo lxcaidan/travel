d6dcf5316927eddfedf438f9eea50c9271c4f563
ice <1764420633@qq.com>
2019-05-03 23:38:30 +0800

    旅游详情页递归组件

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,21 +3,47 @@
         <keep-alive>
             <detail-header></detail-header>
         </keep-alive>
-        
         <detail-banner></detail-banner>
-        <div style="height: 2000px"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data () {
+        return {
+            list : [
+                {
+                    title: "成人票",
+                    children: [{
+                        title: "成人三馆票",
+                        children: [{
+                            title: "承认三馆联票 - 某一连锁店销售"
+                        }]
+                    },{
+                        title: "成人五馆票"
+                    }]
+                },{
+                    title: "学生票"
+                },{
+                    title: "儿童票"
+                },{
+                    title: "特惠票"
+                }
+            ]
+        }
     }
 }
 </script>
